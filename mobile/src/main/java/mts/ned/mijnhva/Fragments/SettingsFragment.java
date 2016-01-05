21fc9bec28bbfe2bb260c170f7e1a22f486a79bd
Mats Otten <mats@kepsi.nl>
2016-01-05 22:22:21 +0100

    start of SettingsFragment,

---
+++ mobile/src/main/java/mts/ned/mijnhva/Fragments/SettingsFragment.java
@@ -0,0 +1,52 @@
+package mts.ned.mijnhva.Fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mts.ned.mijnhva.MainActivity;
+import mts.ned.mijnhva.R;
+
+/**
+ * Created by Mats on 5-1-2016.
+ */
+public class SettingsFragment extends PreferenceFragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	private static final String ARG_SECTION_NUMBER = "section_number";
+
+	/**
+	 * Returns a new instance of this fragment for the given section
+	 * number.
+	 */
+	public static SettingsFragment newInstance(int sectionNumber) {
+		SettingsFragment fragment = new SettingsFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public SettingsFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.settings_fragment);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+	}
+}
